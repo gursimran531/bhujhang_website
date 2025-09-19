bba9d4be885d90e9fc9b6b9060837cfe756f1aa9
Gursimran singh <prince.prince.ty4@gmail.com>
2025-09-19 17:14:21 +0530

    Updated Gallery.tsx

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -19,7 +19,7 @@ const Gallery = () => {
       id: 1,
       type: 'image',
       category: 'gatka',
-      src: '/1.jpg',
+      src: '/8.jpg',
       title: 'Traditional Gatka Training',
       description: 'Master demonstrating traditional Gatka sword techniques',
       location: 'Nawanshahr, India',
