f5d5a3b8ffbb40f697cd4955fbe20628ee29c41b
Gursimran singh <prince.prince.ty4@gmail.com>
2025-09-19 18:41:12 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
             <img 
               src="/logo.png" 
               alt="Bhujhang Jatha Logo" 
-             className="w-20 h-20 rounded-full object-contain shadow-lg"
+             className="w-22 h-22 rounded-full object-contain shadow-lg"
              style={{ imageRendering: 'crisp-edges' }}
             />
             <div>
